b9a82bcfae9b6b5e051aecccf29ffa266325fbc8
François Mathieu <francois.mathieu@beclood.com>
2017-01-11 15:14:38 +0100

    First Commit

---
+++ settings/override/site.ini.append.php
@@ -0,0 +1,37 @@
+<?php /* #?ini charset="utf-8"?
+
+[ExtensionSettings]
+ActiveExtensions[]
+ActiveExtensions[]=ezmultiupload
+ActiveExtensions[]=eztags
+ActiveExtensions[]=ezautosave
+ActiveExtensions[]=ezjscore
+ActiveExtensions[]=ezie
+ActiveExtensions[]=ezoe
+ActiveExtensions[]=ezodf
+ActiveExtensions[]=ezprestapiprovider
+
+[Session]
+SessionNameHandler=custom
+
+[SiteSettings]
+DefaultAccess=user_zone
+SiteList[]
+SiteList[]=user_zone
+
+[SiteAccessSettings]
+CheckValidity=false
+AvailableSiteAccessList[]
+AvailableSiteAccessList[]=user_zone
+AvailableSiteAccessList[]=admin_zone
+MatchOrder=uri
+HostMatchMapItems[]
+
+[DesignSettings]
+DesignLocationCache=enabled
+
+[MailSettings]
+Transport=sendmail
+AdminEmail=francois.mathieu@beclood.com
+EmailSender=
+*/ ?>
---
+++ settings/siteaccess/admin_zone/contentstructuremenu.ini.append.php
@@ -0,0 +1,15 @@
+<?php /* #?ini charset="utf-8"?
+
+[TreeMenu]
+Dynamic=enabled
+ShowClasses[]
+ShowClasses[]=folder
+ShowClasses[]=user_group
+ShowClasses[]=homepage
+ShowClasses[]=annuaire_de_contact
+ShowClasses[]=base_de_connaissance
+ShowClasses[]=contact
+ShowClasses[]=entite
+ShowClasses[]=fiche_pratique
+ShowClasses[]=theme
+*/ ?>
